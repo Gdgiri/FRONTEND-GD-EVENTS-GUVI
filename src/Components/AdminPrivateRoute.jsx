0f76a7313e42939ae5836695f91a31cf9a325a23
giridharan s <160309516+gdharan361@users.noreply.github.com>
2024-10-06 20:05:09 +0530

    update

--- src/Components/AdminPrivateRoute.jsx
+++ src/Components/AdminPrivateRoute.jsx
@@ -3,11 +3,11 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const AdminPrivateRoute = () => {
-  const { user } = useSelector((state) => state.auth);
-  console.log(user);
+  const { auth } = useSelector((state) => state.auth);
+ 
 
   // If the user is an admin, render the Outlet, otherwise navigate to login
-  return user && user.isAdmin === true ? (
+  return auth && auth.isAdmin === true ? (
     <Outlet />
   ) : (
     <Navigate to="/login" /> // Correctly using Navigate here
